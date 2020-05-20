d4f7681c557ab2bc730b5097de1f3c6932ef4a72
HP <nazarova.svet95@gmail.com>
2020-05-20 22:03:16 +0300

    cos sin

--- luck.py
+++ luck.py
@@ -1,4 +1,18 @@
-a = int(input())
-b = int(input())
 
-print(a+b)
+i=0
+while True:
+
+    if i>1000:
+        break
+    print(i)
+    i+=100
+
+
+n = int(input())
+
+factorial = 1
+while n > 1:
+    factorial *= n
+    n -= 1
+
+    print(factorial)
