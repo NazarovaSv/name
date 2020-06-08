1410ce0f701c9a17ee81bd6ce261746baf90fbf9
NazarovaSv <nazarova.svet95@gmail.com>
2020-06-08 13:49:05 +0300

    Исправлено Сравнение дроби

--- day3/1.py
+++ day3/1.py
@@ -1,13 +1,11 @@
-a, n = '1 3', '10 5'
+a, n = '10 5', '10 5'
 al, nl = a.split(), n.split()
 az0, az1, nz0, nz1 = int(al[0]), int(al[1]), int (nl[0]), int (nl[1])
-if az0/az1 == int(az0/az1) and nz0/nz1 == int(nz0/nz1):
-    if az0/az1 > nz0/nz1:
+if az0/az1 > nz0/nz1:
         print(1)
-    elif az0/az1 == nz0/nz1:
+elif az0/az1 == nz0/nz1:
         print(0)
-    else:
-        print(-1)
 else:
-    print('что-то пошло не так')
+        print(-1)
+
 
