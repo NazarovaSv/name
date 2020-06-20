a990d4725b216007dce72c24ec38fb25b8c4272d
NazarovaSv <nazarova.svet95@gmail.com>
2020-06-20 11:26:10 +0300

    Переписать в другой файл все его строки с заменой в них символа 0 на символ 1 и наоборот.

---
+++ day8/task 4.8.py
@@ -0,0 +1,15 @@
+with open('lich1.txt', 'r+', encoding ='utf-8') as d, open('lich2.txt', "w+", encoding ='utf-8') as f:
+    t = d.read()
+    for line in t:
+            if line == '0':
+                f.write('1')
+            elif line =='1':
+                f.write('0')
+            else:
+                f.write(line)
+    # t = f.readline(0).split()
+    # if len(g) == len(t):
+    #     print(len(g))
+    #     print(len(t))
+    #     print('\n')
+
